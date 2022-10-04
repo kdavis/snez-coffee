6e1df0e26aeda14cc55bb5cf6ce85d69bc071bc8
Keir <keir@sneezey.uk>
2022-10-04 20:01:44 +0100

    feat(blog-post): add thumbnail to tags

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { graphql } from 'gatsby'
 import get from 'lodash/get'
 import { renderRichText } from 'gatsby-source-contentful/rich-text'
-import { documentToPlainTextString } from '@contentful/rich-text-plain-text-renderer'
 import { BLOCKS } from '@contentful/rich-text-types'
 import { GatsbyImage } from 'gatsby-plugin-image'
 
@@ -12,9 +11,7 @@ import Seo from '../components/seo'
 class BlogPostTemplate extends React.Component {
   render() {
     const post = get(this.props, 'data.contentfulCoffeePost')
-    const plainTextDescription = documentToPlainTextString(
-      JSON.parse(post.body.raw)
-    )
+    const plainTextDescription = post.overview?.overview
 
     const options = {
       renderNode: {
@@ -34,7 +31,7 @@ class BlogPostTemplate extends React.Component {
         <Seo
           title={post.title}
           description={plainTextDescription}
-          image={`http:${post.thumbnail.resize.src}`}
+          image={post.thumbnail.resize.src}
         />
         <article class="blog-post">
           <h2 class="blog-post-title mb-1">{post.title}</h2>
@@ -52,6 +49,9 @@ export const pageQuery = graphql`
     contentfulCoffeePost(slug: { eq: $slug }) {
       title
       slug
+      overview {
+        overview
+      }
       thumbnail {
         resize(height: 256, width: 256) {
           src
